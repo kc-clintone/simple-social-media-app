818901a697bbed79457cc7f270095d118f2b367e
kc-clintone <kayseeclintone@gmail.com>
2023-10-22 22:57:26 +0000

    modified a few files

--- backend/index.js
+++ backend/index.js
@@ -1,16 +1,32 @@
-const bodyParser = require('body-parser')
-const mongoose = require('mongoose')
-const express = require('express')
-const cors = require('cors')
-const nodemailer = require('nodemailer')
-const crypto = require('crypto')
-const dotenv = require('dotenv')
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const express = require("express");
+const cors = require("cors");
+const nodemailer = require("nodemailer");
+const crypto = require("crypto");
+const dotenv = require("dotenv");
 
 // initialize the app
-const app = express()
+const app = express();
+app.use(dotenv);
+dotenv.config();
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3000;
 
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
-app.use(cors())
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(cors());
+const jwt = require("jsonwebtoken");
+
+mongoose
+  .connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    app.listen(port)
+    console.log(`Database connected successfully, server running on port: ${port}`);
+  })
+  .catch((err) => {
+    console.log("An error occured while connecting. Try again.");
+  });
