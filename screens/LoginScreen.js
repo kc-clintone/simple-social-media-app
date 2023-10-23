96abeae1c58d2d69fe15faa52205929533bb4e5a
kc-clintone <kayseeclintone@gmail.com>
2023-10-23 08:53:35 +0000

    completed the ui for login inputs

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,18 +1,84 @@
-import { SafeAreaView, StyleSheet, Text, View, Image } from "react-native";
-import React from "react";
+import {
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  KeyboardAvoidingView,
+  TextInput,
+} from "react-native";
+import React, { useState } from "react";
+import { Ionicons } from "@expo/vector-icons";
 
 const LoginScreen = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   return (
     <SafeAreaView style={styles.container}>
-      <View style={{ marginTop: 30 }}>
-        <Image
-          style={styles.logo}
-          source={{
-            uri: "https://drive.google.com/file/d/1lw_46L9SSMmEhDg0lsJN4uw1Wz5YH_f2/view?usp=share_link",
-          }}
-        />
+      <View style={{ marginTop: 70 }}>
+        <Image style={styles.logo} source={require("../assets/64.jpg")} />
       </View>
-      <Text>Login to your account</Text>
+      <KeyboardAvoidingView>
+        <View style={styles.title_wrap}>
+          <Text style={styles.title}>Login to your account</Text>
+        </View>
+        <View style={styles.input_wrap}>
+          <View style={styles._input}>
+            <Ionicons
+              style={{ marginLeft: 5 }}
+              name="ios-mail"
+              size={25}
+              color="black"
+            />
+            <TextInput
+              style={{
+                marginVertical: 8,
+                color: "gray",
+                width: 280,
+                fontSize: password ? 16 : 16,
+              }}
+              placeholder="example@mail.com"
+              placeholderTextColor={"gray"}
+              value={email}
+              onChangeText={(text) => setEmail(text)}
+            />
+          </View>
+
+          <View style={styles._input}>
+            <Ionicons
+              style={{ marginLeft: 5 }}
+              name="lock-closed"
+              size={25}
+              color="black"
+            />
+            <TextInput
+              style={{
+                marginVertical: 8,
+                color: "gray",
+                width: 280,
+                fontSize: password ? 16 : 16,
+              }}
+              placeholder="password"
+              placeholderTextColor={"gray"}
+              value={password}
+              onChangeText={(text) => setPassword(text)}
+              secureTextEntry={true}
+            />
+          </View>
+
+          <View style={styles.text_links}>
+            <Text>Keep me logged in</Text>
+            <Text
+              style={{
+                color: "#007FFF",
+                fontWeight: 500,
+              }}
+            >
+              Forgot password
+            </Text>
+          </View>
+        </View>
+      </KeyboardAvoidingView>
     </SafeAreaView>
   );
 };
@@ -29,7 +95,36 @@ const styles = StyleSheet.create({
     width: 100,
     height: 100,
     resizeMode: "contain",
-    borderBlockColor: "red",
-    borderWidth: 3,
+  },
+  title_wrap: {
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  title: {
+    fontWeight: "bold",
+    fontSize: 19,
+    marginTop: 15,
+  },
+  input_wrap: {
+    marginTop: 35,
+  },
+  _input: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    width: "100%",
+    gap: 5,
+    borderColor: "#D0D0D0",
+    borderWidth: 1,
+    borderRadius: 5,
+    padding: 5,
+    marginBottom: 30,
+  },
+  text_links: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    padding: 3,
+    marginTop: -18,
   },
 });
